b1694ecb28f53bd7eec168a6f8af11893c3d2938
cherylpinto <cp16november@gmail.com>
2025-01-19 19:14:34 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -9,7 +9,29 @@ import Release from "./components/Release";
 import Signup from "./components/Signup";
 import Footer from "./components/Footer";
 import ScrollToTop from "./components/ScrollToTop";
+import {useEffect} from "react"
+import scrollreveal from "scrollreveal"
 function App() {
+  useEffect(() => {
+    const registerAnimations=()=>{
+      const sr=scrollreveal({
+        origin:"bottom",
+        distance:"80px",
+        duration:1500,
+        reset:false,
+      })
+      sr.reveal(`nav, .home, .free, .client, .super-rare,.releases, .like, .signup,footer`,{interval:500})
+    }
+  
+    registerAnimations();
+  }, [])
+  window.setTimeout(()=>{
+    const home=document.getElementsByClassName("home");
+    home[0].style.transform="none";
+    const nav=document.getElementsByTagName("nav");
+    nav[0].style.transform="none";
+  },1000);
+  
   return (
     <div className="app-container">
       <Navbar/>
